72f08d8dbad2d057433ad0814ef2eff21497bca4
Joseph <JosephSped23@gmail.com>
2023-11-19 15:38:58 -0500

    Used to Dom to create functions of task list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ document.querySelector('form').addEventListener('submit', (e) =>{
+    (e).preventDefault()
+   handleTask(e.target.new_task_description.value)
+ 
+  })
+})
+
+function handleTask(task){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  btn.addEventListener('click', handleDelete)
+  p.textContent = task
+  p.appendChild(btn)
+  console.log(p)
+
+  document.querySelector('#tasks').appendChild(p)
+ 
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
